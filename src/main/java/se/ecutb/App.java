ed493cac49a9274c931d7639757f47a929b2f026
Erik Svensson <eosvensson@gmail.com>
2020-01-16 12:26:15 +0100

    Finished console branch

--- src/main/java/se/ecutb/App.java
+++ src/main/java/se/ecutb/App.java
@@ -8,6 +8,7 @@ import se.ecutb.model.Person;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Comparator;
+import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -16,27 +17,69 @@ public class App
 {
 
     private static DataStorage dataStorage;
+    private static Scanner scanner;
 
     static {
         dataStorage = DataStorage.INSTANCE;
+        scanner = new Scanner(System.in);
     }
 
     public static void main( String[] args )
     {
+        boolean done = false;
         String message = "Running exercise...";
-        exercise1(message + "1");
-        exercise2(message + "2");
-        exercise3(message + "3");
-        exercise4(message + "4");
-        exercise5(message + "5");
-        exercise6(message + "6");
-        exercise7(message + "7");
-        exercise8(message + "8");
-        exercise9(message + "9");
-        exercise10(message + "10");
-        exercise11(message + "11");
-        exercise12(message + "12");
-        exercise13(message + "13");
+        do{
+            System.out.println(menu());
+            System.out.print("Choose from 1-13: ");
+            int number = getInt();
+            switch (number){
+                case 0:
+                    done = true;
+                    break;
+                case 1:
+                    exercise1(message + number);
+                    break;
+                case 2:
+                    exercise2(message + number);
+                    break;
+                case 3:
+                    exercise3(message + number);
+                    break;
+                case 4:
+                    exercise4(message + number);
+                    break;
+                case 5:
+                    exercise5(message + number);
+                    break;
+                case 6:
+                    exercise6(message + number);
+                    break;
+                case 7:
+                    exercise7(message + number);
+                    break;
+                case 8:
+                    exercise8(message + number);
+                    break;
+                case 9:
+                    exercise9(message + number);
+                    break;
+                case 10:
+                    exercise10(message + number);
+                    break;
+                case 11:
+                    exercise11(message + number);
+                    break;
+                case 12:
+                    exercise12(message + number);
+                    break;
+                case 13:
+                    exercise13(message + number);
+                    break;
+            }
+
+
+        }while(!done);
+        scanner.close();
     }
 
     /*
@@ -81,10 +124,12 @@ public class App
 
     /*
         5.	Find the Person that has an id of 456 and convert to String with following content:
+            “Name: Nisse Nilsson born 1999-09-09”. Use findOneAndMapToString().
      */
     public static void exercise5(String message){
         System.out.println(message);
-        System.out.println(dataStorage.findOneAndMapToString(person -> person.getId() == 456, Person::toString));
+        Function<Person, String> personStringFunction = person -> "Name: " + person.getFirstName() + " " + person.getLastName() + " born " + person.getBirthDate();
+        System.out.println(dataStorage.findOneAndMapToString(person -> person.getId() == 456, personStringFunction));
         System.out.println("----------------------");
     }
 
@@ -188,6 +233,45 @@ public class App
         dataStorage.findAndSort(chain)
             .forEach(System.out::println);
         System.out.println("----------------------");
+    }
+
+    public static String menu(){
+        StringBuilder stringBuilder = new StringBuilder("Please make a choice: \n");
+        stringBuilder.append("0.\tQuit\n");
+        stringBuilder.append("1.\tFind everyone that has firstName: “Erik” using findMany().\n");
+        stringBuilder.append("2.\tFind all females in the collection using findMany().\n");
+        stringBuilder.append("3.\tFind all who are born after (and including) 2000-01-01 using findMany().\n");
+        stringBuilder.append("4.\tFind the Person that has an id of 123 using findOne().\n");
+        stringBuilder.append("5.\tFind the Person that has an id of 456 and convert to String with following content:\n" +
+                "            “Name: Nisse Nilsson born 1999-09-09”. Use findOneAndMapToString().\n");
+        stringBuilder.append("6.\tFind all male people whose names start with “E” and convert each to a String using findManyAndMapEachToString().\n");
+        stringBuilder.append("7.\tFind all people who are below age of 10 and convert them to a String like this:\n" +
+                "            “Olle Svensson 9 years”. Use findManyAndMapEachToString() method.\n");
+        stringBuilder.append("8.\tUsing findAndDo() print out all people with firstName “Ulf”.\n");
+        stringBuilder.append("9.\tUsing findAndDo() print out everyone who have their lastName contain their firstName.\n");
+        stringBuilder.append("10.\tUsing findAndDo() print out the firstName and lastName of everyone whose firstName is a palindrome.\n");
+        stringBuilder.append("11.\tUsing findAndSort() find everyone whose firstName starts with A sorted by birthDate.\n");
+        stringBuilder.append("12.\tUsing findAndSort() find everyone born before 1950 sorted reversed by lastest to earliest.\n");
+        stringBuilder.append("13.\tUsing findAndSort() find everyone sorted in following order: lastName > firstName > birthDate.\n");
+        return stringBuilder.toString();
+    }
+
+    public static int getInt(){
+        int number = 0;
+        boolean validNumber = false;
+        while(!validNumber){
+            try{
+                number = Integer.parseInt(scanner.nextLine().trim());
+                validNumber = number >= 0 && number < 14 ? true : false;
+            }catch (NumberFormatException ex){
+                System.out.println("Not a integer");
+            }
+            if(!validNumber){
+                System.out.println("Try again");
+            }
+        }
+
+        return number;
     }
 
 
